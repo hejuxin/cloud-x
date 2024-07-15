d23967e85472398c8d3b2e529b3dff67ac25599d
hejuxin <cuobingfeicuo@126.com>
2024-07-15 12:59:26 +0800

    feat: 组件库测试

---
+++ components/button/button.tsx
@@ -0,0 +1,13 @@
+import React, { ButtonHTMLAttributes } from 'react';
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
+
+const Button: React.FC<ButtonProps> = props => {
+  const { children } = props;
+  return (
+    <button type='button'>{children}</button>
+  )
+}
+
+export default Button;
+export type { ButtonProps };
