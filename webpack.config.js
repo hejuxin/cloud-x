fd519078eea3ee399fa329ce89f6862d5e2cb9b9
hejuxin <cuobingfeicuo@126.com>
2024-07-15 11:57:27 +0800

    feat: 配置外部扩展

--- webpack.config.js
+++ webpack.config.js
@@ -83,4 +83,18 @@ module.exports = {
       filename: '[name].css',
     }),
   ],
+  externals: {
+    react: {
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react',
+    },
+    'react-dom': {
+      root: 'ReactDOM',
+      commonjs2: 'react-dom',
+      commonjs: 'react-dom',
+      amd: 'react-dom',
+    },
+  },
 }
